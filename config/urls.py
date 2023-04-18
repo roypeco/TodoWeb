1a19b45373d66b6b82f490de171cf36adf7d4869
roypeco <roy.peco@gmail.com>
2023-04-19 01:14:33 +0900

    本日終了

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("todo/", include("todo.urls")),
     path("calc/", include("calc.urls")),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from .models import Todo
 
+
 class TodoList(ListView):
     model = Todo
     context_object_name = "tasks"
@@ -25,3 +26,4 @@ class TodoDelete(DeleteView):
     model = Todo
     context_object_name = "task"
     success_url = reverse_lazy("list")
+    
